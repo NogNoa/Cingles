b60380319cdcb4dd10c5a2e21265ee5dc0bd253d
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-04 16:07:46 +0200

    by 1 correctness

--- rope.lb.c
+++ rope.lb.c
@@ -8,17 +8,17 @@ rope rope_get(const char* scroll)
     const int total_len=strlen(scroll);
     const int full_cells=total_len / 0x100;
     rope* last = malloc(sizeof(rope));
-    *last = (rope) {total_len % 0x100, NULL, NULL};
-    const int last_len = total_len % 0xFF;
-    last->str = malloc(last_len);
+    const int last_len = total_len % 0x100;
+    *last = (rope) {last_len, NULL, NULL};
+    last->str = malloc(last_len+1);
     strncpy(last->str, scroll+(full_cells * 0x100), last_len);
     rope* nxt=last;
-    for (int cell=full_cells-1; cell>0; cell--)
+    for (int cell=full_cells; cell>0; cell--)
     {   rope* curr =malloc(sizeof(rope));
         *curr = (rope) {0xFF, nxt, NULL};
-        curr->str = malloc(0xFF);
-        strncpy(curr->str, scroll+(cell * 0x100), 0xFF);
+        curr->str = malloc(0x100);
+        strncpy(curr->str, scroll+((cell-1) * 0x100), 0xFF);
         nxt = curr;
     }
-    return *last;
+    return *nxt;
 }
--- try.c
+++ try.c
@@ -5,4 +5,9 @@ int main(int argc, char *argv[])
 {
     rope a = rope_get("hello");
     puts(a.str);
+    char b[0x102];
+    for (int i=0;i<0x101;i++)
+        b[i]='$';
+    b[0x101]='\0';
+    rope c = rope_get(b);
 }
