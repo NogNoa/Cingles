059cedfd9d9252b96184e436d3099f1e5e9c6b78
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-04 14:54:11 +0200

    allocate pointers

--- rope.lb.c
+++ rope.lb.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include "rope.lb.h"
 
 
@@ -6,9 +7,10 @@ rope rope_get(char* str)
 {
     int total_len=strlen(str);
     int n_cells=total_len / 0x100;
-    
+    rope* first=malloc(sizeof(rope));
+    rope* prev=first;
     for (int i_cl=0;i_cl<n_cells-1;i_cl++)
-    {   
+    {   rope* curr =
 
     }
 
