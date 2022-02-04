32463c0d7006e9e09eeff675320077d42e6f84a8
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-04 15:02:38 +0200

    start from end

--- rope.lb.c
+++ rope.lb.c
@@ -3,14 +3,16 @@
 #include "rope.lb.h"
 
 
-rope rope_get(char* str)
+rope rope_get(char* scroll)
 {
-    int total_len=strlen(str);
-    int n_cells=total_len / 0x100;
-    rope* first=malloc(sizeof(rope));
-    rope* prev=first;
+    int total_len=strlen(scroll);
+    int n_cells=total_len / 0x100 + 1;
+    rope* last = malloc(sizeof(rope));
+    *last = {total_len % 0x100, NULL, NULL};
+    strncpy(last->str, scroll[(n_cells - 1) * 0x100], total_len % 0x100);
+    rope* nxt=last;
     for (int i_cl=0;i_cl<n_cells-1;i_cl++)
-    {   rope* curr =
+    {   rope* curr =malloc(sizeof(rope));
 
     }
 
