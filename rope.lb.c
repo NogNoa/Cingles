4c1cd69c9f51c32d43fd0996f04cab65929b930a
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-04 15:22:07 +0200

    fix compiler complaints

--- rope.lb.c
+++ rope.lb.c
@@ -8,13 +8,13 @@ rope rope_get(char* scroll)
     int total_len=strlen(scroll);
     int full_cells=total_len / 0x100;
     rope* last = malloc(sizeof(rope));
-    *last = {total_len % 0x100, NULL, NULL};
-    strncpy(last->str, scroll[full_cells * 0x100], total_len % 0x100);
+    *last = (rope) {total_len % 0x100, NULL, NULL};
+    strncpy(last->str, scroll+(full_cells * 0x100), total_len % 0xFF);
     rope* nxt=last;
     for (int cell=full_cells-1; cell>=0; cell--)
     {   rope* curr =malloc(sizeof(rope));
-        *last = {0x100, nxt, NULL};
-        strncpy(last->str, scroll[cell * 0x100], 0x100);
+        *last = (rope) {0xFF, nxt, NULL};
+        strncpy(last->str, scroll+(cell * 0x100), 0xFF);
         nxt = curr;
     }
     return *last;
